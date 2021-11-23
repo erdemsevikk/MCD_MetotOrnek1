c9bef88043c56fe1296f0055cebd656a44a1d4b7
erdemsevikk <erdemsevikk@gmail.com>
2021-11-23 16:52:37 +0300

    düzenlendi

--- MCD_MetotOrnek1/Program.cs
+++ MCD_MetotOrnek1/Program.cs
@@ -10,6 +10,44 @@ namespace MCD_MetotOrnek1
     {
         static void Main(string[] args)
         {
+            EkranaMerhabaYaz();
+            EkranaMerhabaYaz();
+            EkranaMerhabaYaz();
+            EkranaMerhabaYaz();
+            EkranaGonderilenIsimIleMerhabaYaz("erdem");
+            EkranaGonderilenIsimIleMerhabaYaz("şevik");
+            EkranaGonderilenIsimleKacTaneMerhabaYaz("Erdem", 5);
+
+            Console.ReadLine();
         }
+
+
+        private static void EkranaMerhabaYaz()
+        {
+            Console.WriteLine("Merhaba");
+        }
+
+        private static void EkranaGonderilenIsimIleMerhabaYaz(string Isim)
+        {
+            Console.WriteLine("Merhaba sayın : " +Isim.ToUpper());
+        }
+
+        private static  void EkranaGonderilenIsimleKacTaneMerhabaYaz(string Isim, int KacTane)
+        {
+            if (KacTane > 0)
+            {
+                for (int i = 0; i < KacTane; i++)
+                {
+                    Console.WriteLine("Merhaba sayın : " + Isim.ToUpper());
+                }
+            }
+
+            else if(KacTane <= 0)
+            {
+                Console.WriteLine("Lütfen pozitif sayı giriniz.");
+            }
+        }
+
+
     }
 }
